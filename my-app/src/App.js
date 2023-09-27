90e6c48623fa32983b502cd121bc160ed051751f
vladddyslav <vladvolpl@gmail.com>
2023-09-27 19:22:43 +0200

    init2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Header from './components/header/Header';
 
 
 function App() {
   return (
-    <div className='container'>
-    
-    </div>
+    <Header/>
   );
 }
 
--- my-app/src/components/header/Header.js
+++ my-app/src/components/header/Header.js
@@ -2,7 +2,22 @@ import './Header.scss'
 
 const Header = () => {
    return (
-      <>
-      </>
+      <div className='header'>
+         <div className="top">
+            <div className="logo">
+               <div className="logoImg"></div>
+               <div className="logoTxt"></div>
+            </div>
+            <div className="nav">
+               <button className='navBtn'>MY LIBRARY</button>
+               <button className='navBtn'>HOME</button>
+            </div>
+         </div>
+         <div className="searchPanel">
+            <input className="searchInput" type='search' placeholder='Movie search'  />
+         </div>
+      </div>
    )
 }
+
+export default Header;
